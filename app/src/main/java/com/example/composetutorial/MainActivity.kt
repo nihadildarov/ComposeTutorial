44f85cb39b0250d156b304ab669fa365529fb177
Nihad Ildarov <nildarov077@gmail.com>
2024-08-26 13:48:33 +0400

    Sample list and addition created. Reusable ui elements created

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -4,11 +4,24 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.material3.Button
+import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,7 +32,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
@@ -28,27 +43,69 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTutorialTheme {
-                var count by remember{
-                    mutableStateOf(0)
+                var name by remember {
+                    mutableStateOf("")
                 }
+                var names by remember {
+                    mutableStateOf(emptyList<String>())
+                }
+
                 Column(
-                    modifier = Modifier.fillMaxSize(),
-                   verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
                 ) {
-                    Text(
-                        text = count.toString(),
-                        fontSize = 30.sp
-                    )
-                    Button(
-                        onClick = {
-                            count++
-                        }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
                     ) {
-                        Text(text = "Click Me! $count")
+                        OutlinedTextField(
+                            value = name,
+                            onValueChange = { text ->
+                                name = text
+                            },
+                            modifier = Modifier
+                                .weight(1f)
+                        )
+                        Spacer(modifier = Modifier.width(7.dp))
+                        Button(
+                            onClick = {
+                                if (name.isNotBlank()) {
+                                    names = names + name
+                                }
+                            }) {
+                            Text(text = "Add")
+                        }
                     }
+
+                    NameList(names = names)
+
                 }
             }
+        }
+    }
+}
+
+
+@Composable
+fun NameList(
+    names: List<String>,
+    modifier: Modifier = Modifier
+) {
+
+    LazyColumn(
+        modifier
+    ) {
+        items(names) { currentName ->
+            Text(
+                text = currentName,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.DarkGray)
+                    .padding(16.dp),
+                color = Color.White
+            )
+            VerticalDivider(modifier = Modifier.height(5.dp), color = Color.Transparent)
         }
     }
 }
