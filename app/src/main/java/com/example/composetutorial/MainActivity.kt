fb2e262fae8662df3bbbfdb8be8b979b0f14fa01
Nihad Ildarov <nildarov077@gmail.com>
2024-08-30 12:46:30 +0400

    updated

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -5,72 +5,127 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
-import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
-import androidx.compose.material3.rememberBottomSheetScaffoldState
-import androidx.compose.runtime.remember
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 import kotlinx.coroutines.launch
+import kotlin.random.Random
+import kotlin.reflect.typeOf
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             ComposeTutorialTheme {
                 val snackBarHostState = remember {
                     SnackbarHostState()
+                }
+
+                var textFieldState by remember {
+                    mutableStateOf("")
                 }
                 val snackBarScope = rememberCoroutineScope()
                 Scaffold(
                     modifier = Modifier
                         .fillMaxSize(),
                     snackbarHost = {
-                        SnackbarHost(snackBarHostState){data->
-                            Snackbar (containerColor = Color.Black, actionColor = Color.White, snackbarData = data)
+                        SnackbarHost(snackBarHostState) { data ->
+                            Snackbar(
+                                containerColor = Color.Black,
+                                actionColor = Color.White,
+                                snackbarData = data
+                            )
                         }
                     }
                 ) {
-                    Column {
-                        Box(modifier = Modifier
+                    Column(
+                        modifier = Modifier
                             .fillMaxSize()
-                            .background(Color.Red)
                             .padding(it)
-                            .clickable {
-                                snackBarScope.launch {
-                                    val snackBarResult = snackBarHostState.showSnackbar(
-                                        message = "Click recognized 🧐",
-                                        duration = SnackbarDuration.Short,
-                                        actionLabel = "Retry"
-                                    )
+                            ,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
 
-                                    if (snackBarResult == SnackbarResult.ActionPerformed) {
-                                        snackBarHostState.showSnackbar(message = "Clicked TWIICEE!!! 👏")
-                                    }
-                                }
-                            }
+                        OutlinedTextField(
+                            value = textFieldState,
+                            label = {
+                                Text(text = "Enter name")
+                            },
+                            onValueChange = {
+                                textFieldState = it
+                            },
+                            singleLine = true,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 10.dp)
                         )
 
+
+                            Spacer(modifier = Modifier
+                                .height(4.dp)
+                                .fillMaxWidth())
+                            Button(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 10.dp),
+                                onClick = {
+                                    snackBarScope.launch {
+                                        val snackBarResult = snackBarHostState.showSnackbar(
+                                            message = "Congratulations! $textFieldState has created!",
+                                            withDismissAction = true,
+                                            actionLabel = "Cancel")
+
+                                        if(snackBarResult == SnackbarResult.ActionPerformed){
+                                            snackBarHostState.showSnackbar("Action Canceled.")
+                                        }
+                                    }
+                                }
+                            ) {
+                                Text(
+                                    text = if (textFieldState.isBlank()) {
+                                    "Create"
+                                } else {
+                                    "Create \"$textFieldState\""
+                                }
+                                )
+                        }
                     }
                 }
-
-
             }
         }
     }
 }
+
+
+
