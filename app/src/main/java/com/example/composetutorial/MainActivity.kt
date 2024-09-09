3a3e6431bb46ee581822d4b8beaa2b09f5c08297
Nihad Ildarov <nildarov077@gmail.com>
2024-09-09 13:26:47 +0400

    updated volumeBar and MusicKnob

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -2,18 +2,30 @@ package com.example.composetutorial
 
 import android.health.connect.datatypes.units.Percentage
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -30,11 +42,19 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Canvas
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -45,42 +65,136 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
+import kotlin.math.atan2
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ComposeTutorialTheme {
-                var percentageState by remember {
-                    mutableFloatStateOf(0f)
-                }
-
-                Box(
-                    contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFF101010))
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .border(1.dp, Color.Green, RoundedCornerShape(10.dp))
+                        .padding(30.dp)
                 ) {
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        CircularProgressBar(
-                            percentage = percentageState,
-                            number = 100,
-                            color = if (percentageState<=0.2f){
-                                Color.Red
-                            } else {
-                                Color.Green
-                            })
-                        percentageState = percentageInput()
-
-                        Button(onClick = {
-                            percentageState += 0.1f
-                        }) {
-                            Text("Increase by 10%")
-                        }
+                    var volume by remember {
+                        mutableFloatStateOf(0f)
                     }
+
+                    val barCount = 20
+                    MusicKnob(modifier = Modifier.size(100.dp)) {
+                        volume = it
+                        println("The volume is $it")
+                    }
+
+                    Spacer(modifier = Modifier.width(20.dp))
+                    VolumeBar(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(35.dp),
+                        activeBars = (barCount * volume).roundToInt(),
+                        barCount = barCount
+                    )
                 }
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun MusicKnob(
+    modifier: Modifier = Modifier,
+    limitingAngle: Float = 25f,
+    onValueChange: (Float) -> Unit
+) {
+    var rotation by remember {
+        mutableFloatStateOf(limitingAngle)
+    }
+
+    var touchX by remember {
+        mutableFloatStateOf(0f)
+    }
+    var touchY by remember {
+        mutableFloatStateOf(0f)
+    }
+    var centerX by remember {
+        mutableFloatStateOf(0f)
+    }
+    var centerY by remember {
+        mutableFloatStateOf(0f)
+    }
+
+    Image(
+        painter = painterResource(R.drawable.music_knob),
+        contentDescription = "Music knob",
+        modifier = modifier
+            .fillMaxSize()
+            .onGloballyPositioned {
+                val windowBounds = it.boundsInWindow()
+                centerX = windowBounds.size.width / 2f
+                centerY = windowBounds.size.height / 2f
+            }
+            .pointerInteropFilter { event ->
+                touchX = event.x
+                touchY = event.y
+                val angle = -atan2(centerX - touchX, centerY - touchY) * (180f / Math.PI).toFloat()
+
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN,
+                    MotionEvent.ACTION_MOVE -> {
+                        if (angle !in -limitingAngle..limitingAngle) {
+                            val fixedAngle = if (angle in -180f..-limitingAngle) {
+                                360f + angle
+                            } else {
+                                angle
+                            }
+                            rotation = fixedAngle
+
+                            val percentage =
+                                (fixedAngle - limitingAngle) / (360f - 2 * limitingAngle)
+                            onValueChange(percentage)
+                            true
+                        } else false
+                    }
+
+                    else -> false
+                }
+            }
+            .rotate(rotation)
+    )
+}
+@Composable
+fun VolumeBar(
+    modifier: Modifier = Modifier,
+    activeBars: Int = 0,
+    barCount: Int = 20
+) {
+    BoxWithConstraints(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+    ) {
+        val barWidth = remember {
+            constraints.maxWidth / (2f * barCount)
+        }
+        Canvas(modifier = modifier) {
+            for(i in 0 until barCount) {
+                drawRoundRect(
+                    color = if(i in 0..activeBars) Color.Green else Color.DarkGray,
+                    topLeft = Offset(i * barWidth * 2f + barWidth / 2f, 0f),
+                    size = Size(barWidth, constraints.maxHeight.toFloat()),
+                    cornerRadius = CornerRadius(0f)
+                )
             }
         }
     }
@@ -106,7 +220,7 @@ fun CircularProgressBar(
         targetValue = if (animationPlayed) percentage else 0f, animationSpec = tween(
             durationMillis = 3000,
             delayMillis = 1000,
-        )
+        ), label = ""
     )
 
     LaunchedEffect(key1 = true) {
@@ -116,7 +230,7 @@ fun CircularProgressBar(
     Box(
         contentAlignment = Alignment.Center, modifier = Modifier.size(radius * 2f)
     ) {
-        androidx.compose.foundation.Canvas(modifier = Modifier.size(radius * 2f)) {
+        Canvas(modifier = Modifier.size(radius * 2f)) {
             drawArc(
                 color = color,
                 startAngle = -90f,
@@ -128,7 +242,7 @@ fun CircularProgressBar(
             )
         }
         Text(
-            text = (curPercentage.value * number).toInt().toString()+ "%",
+            text = (curPercentage.value * number).toInt().toString() + "%",
             color = Color.Black,
             fontSize = fontSize,
             fontWeight = FontWeight.Bold
@@ -138,28 +252,26 @@ fun CircularProgressBar(
 }
 
 
-
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun percentageInput():Float {
+fun percentageInput(): Float {
     var input by remember { mutableStateOf("") }
-    var percentage by remember { mutableStateOf(0f) }
+    var percentage by remember { mutableFloatStateOf(0f) }
     var errorMessage by remember { mutableStateOf("") }
 
 
-    fun validateAndConvertInput (newValue:String){
+    fun validateAndConvertInput(newValue: String) {
         if (newValue.isEmpty()) {
             input = newValue
             percentage = 0f
             errorMessage = ""
-        }else {
+        } else {
             val number = newValue.toIntOrNull()
-            if (number!= null && number in 0..100){
+            if (number != null && number in 0..100) {
                 input = newValue
-                percentage = number/100f
+                percentage = number / 100f
                 errorMessage = ""
-            }else {
+            } else {
                 errorMessage = "Please enter a valid number between 0 and 100"
             }
         }
@@ -192,7 +304,9 @@ fun percentageInput():Float {
         Text(
 
             text = "Percentage: ${percentage * 100}%",
-            modifier = Modifier.padding(vertical = 16.dp).fillMaxWidth(),
+            modifier = Modifier
+                .padding(vertical = 16.dp)
+                .fillMaxWidth(),
             textAlign = TextAlign.Center
         )
     }
